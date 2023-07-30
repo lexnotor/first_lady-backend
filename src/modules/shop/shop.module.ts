b538cd69469dda0599e79a23231642987e094151
alexandre-notor <amanichambu48@gmail.com>
2023-07-30 21:22:24 +0200

    lint: add shop entity to typeorm

--- src/modules/shop/shop.module.ts
+++ src/modules/shop/shop.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { ShopController } from "./shop.controller";
+import { ShopEntity } from "./shop.entity";
 import { ShopService } from "./shop.service";
 
 @Module({
+    imports: [TypeOrmModule.forFeature([ShopEntity])],
     controllers: [ShopController],
     providers: [ShopService],
 })
