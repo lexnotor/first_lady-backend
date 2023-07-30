e126a9d06ad780592db3b997071276eddc65181b
alexandre-notor <amanichambu48@gmail.com>
2023-07-30 20:49:13 +0200

    feat: init module shop

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from "@nestjs/common";
+import { ShopModule } from "./modules/shop/shop.module";
 
 @Module({
-    imports: [],
+    imports: [ShopModule],
     controllers: [],
     providers: [],
 })
