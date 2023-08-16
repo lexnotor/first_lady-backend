544275cabd2e238cc156424817f57bc19a006e0a
alexandre-notor <amanichambu48@gmail.com>
2023-08-16 13:50:44 +0200

    feat: restrit puppeteer

--- src/modules/facture/facture.module.ts
+++ src/modules/facture/facture.module.ts
@@ -1,12 +1,10 @@
 import { Module } from "@nestjs/common";
-import { OrderModule } from "../order/order.module";
-import { FactureController } from "./facture.controller";
-import { FactureService } from "./facture.service";
 import { ConfigModule } from "@nestjs/config";
+import { OrderModule } from "../order/order.module";
 
 @Module({
     imports: [OrderModule, ConfigModule],
-    controllers: [FactureController],
-    providers: [FactureService],
+    // controllers: [FactureController],
+    // providers: [FactureService],
 })
 export class FactureModule {}
